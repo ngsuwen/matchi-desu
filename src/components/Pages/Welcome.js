a124498a47d43ef58c946579e8ceb20f62d66de4
LAPTOP-KH9KVP83\Suwen <ng.suwen@dhs.sg>
2021-11-11 22:55:21 +0800

    added image to welcome page

--- src/components/Pages/Welcome.js
+++ src/components/Pages/Welcome.js
@@ -16,25 +16,37 @@ export default function Welcome() {
     return (
         <Box sx={{
             display: 'flex',
-            height: '80vh',
+            height: '100vh',
             alignItems: 'center'
         }}>
-            <Typography sx={{ fontFamily: 'damion, cursive', fontWeight: 'bold' }} variant='h3'>
-                Welcome {localStorage.getItem('name') ?
-                    <>
-                        {localStorage.getItem('name')}<br />
-                        <Link style={{textDecoration:'none'}} to ='/main'><Button sx={{ width: '20vw' }} variant="contained" onClick={existingHandler}>Enter</Button></Link>
-                    </> : 
-                    <>
-                        <Typography sx={{ fontFamily: 'damion, cursive', marginBottom: 3 }} variant='h4'>
-                            Please enter your name:
-                        </Typography>
-                        <TextField sx={{ width: '20vw' }} variant="filled" label="Name" inputRef={userName}/><br />
-                        <Link style={{textDecoration:'none'}} to ='/main'><Button sx={{ width: '20vw' }} variant="contained" onClick={newHandler}>Enter</Button></Link>
-                    </>
-                }
-            </Typography>
-
+            <Box sx={{
+                display: 'flex',
+                alignItems: 'center',
+                backgroundColor: 'white',
+                borderRadius: 3,
+                paddingRight: '30px',
+                boxShadow: '0px 0px 30px 0px rgba(0,0,0,0.50)' 
+            }}>
+                <img style={{
+                    padding: 30,
+                    width: 400,
+                }} src='https://i.redd.it/46p3tv5ryqy71.jpg' alt='waifu is here' />
+                <Typography sx={{ fontFamily: 'damion, cursive', fontWeight: 'bold' }} variant='h3'>
+                    Welcome {localStorage.getItem('name') ?
+                        <>
+                            {localStorage.getItem('name')}<br />
+                            <Link style={{ textDecoration: 'none' }} to='/main'><Button sx={{ width: '20vw' }} variant="contained" onClick={existingHandler}>Enter</Button></Link>
+                        </> :
+                        <>
+                            <Typography sx={{ fontFamily: 'damion, cursive', marginBottom: 3 }} variant='h4'>
+                                Please enter your name:
+                            </Typography>
+                            <TextField sx={{ width: '20vw' }} variant="filled" label="Name" inputRef={userName} /><br />
+                            <Link style={{ textDecoration: 'none' }} to='/main'><Button sx={{ width: '20vw' }} variant="contained" onClick={newHandler}>Enter</Button></Link>
+                        </>
+                    }
+                </Typography>
+            </Box>
         </Box>
     )
 }
